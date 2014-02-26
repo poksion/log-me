8b34d4b524ff3b00299cf39c056dcdb81361a7fb
poksion <poksion@gmail.com>
2014-02-26 11:38:23 +0900

    consuming-pattern을 track-me로 이동

--- consuming-pattern/category_policy.rb
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-
-class CategoryPolicy
-    def on_init( category_manager )
-    end
-    
-    def on_add_money( category_manager, category_year, category_month, card_type, used_date, used_content, used_money, dis_count, point)
-    end
-end
