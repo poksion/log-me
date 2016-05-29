d9d3a9c0475be3b2e08dad04481c953e1148565a
poksion <poksion@gmail.com>
2016-05-30 08:53:01 +0900

    adding win32 support

---
+++ worklog/do_log.rb
@@ -0,0 +1,76 @@
+require 'Win32API'
+require 'win32ole'
+require 'socket'
+
+def do_log
+  active_window_handle = get_active_window_handle
+
+  title = get_title(active_window_handle)
+  proc_name = get_proc_name(active_window_handle)
+  
+  ssid = get_ssid
+  ip_addr = get_ip
+  date = get_date
+  
+  "a4 #{ip_addr} #{ssid} #{date} #{proc_name}: #{title}"
+end
+
+def get_active_window_handle
+  getForegroundWindow = Win32API.new('user32', 'GetForegroundWindow', [], 'N')
+  getForegroundWindow.Call
+end
+
+def get_title(window_handle)
+  getWindowTextLength =  Win32API.new('user32', 'GetWindowTextLength', ['L'], 'I')
+  buf_len = getWindowTextLength.Call(window_handle)
+  title_buf = ' ' * (buf_len+1)
+  getWindowText = Win32API.new('user32', 'GetWindowText', ['L', 'P', 'I'], 'I')
+  getWindowText.Call(window_handle, title_buf, title_buf.length)
+  
+  title_buf
+end
+
+def get_proc_name(window_handle)
+  getWindowThreadProcessId = Win32API.new('user32', 'GetWindowThreadProcessId', ['L', 'P'], 'L')
+  pid = [0].pack('L')
+  getWindowThreadProcessId.Call(window_handle, pid)
+  pid = pid.unpack('L')[0]
+  
+  get_proc_name_from_pid(pid)
+end
+
+def get_proc_name_from_pid(pid)
+  procs = WIN32OLE.connect("winmgmts:\\\\.")
+  procs.InstancesOf("win32_process").each do |p|
+    if pid == p.processId
+      return p.name
+    end
+  end
+  return "-"
+end
+
+def get_date
+  Time.now.strftime("%Y/%m/%d-%H:%M")
+end
+
+def get_ssid
+  results = `netsh wlan show interfaces`.split( /\r?\n/ )
+  results.each do |item|
+    if item.include?('SSID')
+      return item.sub(/.*SSID.*: /, '')
+    end
+  end
+  return "-"
+end
+
+def get_ip
+  Socket.ip_address_list.each do |ip|
+    if ip.ipv4? and not ip.ipv4_loopback?
+      return ip.ip_address
+    end
+  end
+  return "-"
+end
+
+#-- <format-id> <ip> <ssid> <date> <appname> <window-title>
+#"a4 " & theIp & " " & theSsid & " " & theDate & " " & frontApp & ": " & window_title
--- worklog/worklog.rb
+++ worklog/worklog.rb
@@ -1,16 +1,45 @@
-
-def check_login
-  login_cnt = `uptime | grep -E -o "[0-9]+ users" | awk '{print $1}'`
-  return false if login_cnt.eql? "0\n"
-  
-  sleep_state = `/usr/sbin/ioreg -n IODisplayWrangler | grep -o \\"CurrentPowerState\\"=4`
-  return false if sleep_state.empty?
-
-  return true
+class WindowLogger
+    def log
+      require_relative 'do_log'
+      File.open('C:\Users\poksi\workspace\worklog.log', 'a+') do |f|
+        f.puts do_log
+      end
+    end
 end
 
-if __FILE__ == $0
-  if check_login
-    puts `osascript ~/workspace/log-me/worklog/worklog.osa`
+class MacLogger
+
+  def check_login
+    login_cnt = `uptime | grep -E -o "[0-9]+ users" | awk '{print $1}'`
+    return false if login_cnt.eql? "0\n"
+      
+    sleep_state = `/usr/sbin/ioreg -n IODisplayWrangler | grep -o \\"CurrentPowerState\\"=4`
+    return false if sleep_state.empty?
+
+    return true
   end
+
+  def log
+    if check_login
+      File.open('~/workspace/worklog.log', 'a+') do |f|
+        f.puts `osascript ~/workspace/log-me/worklog/do_log.osa`
+      end
+    end
+  end
+
+end
+
+#def check_login
+#  login_cnt = `uptime | grep -E -o "[0-9]+ users" | awk '{print $1}'`
+#  return false if login_cnt.eql? "0\n"
+#  
+#  sleep_state = `/usr/sbin/ioreg -n IODisplayWrangler | grep -o \\"CurrentPowerState\\"=4`
+#  return false if sleep_state.empty?
+#
+#  return true
+#end
+
+if __FILE__ == $0
+  logger = WindowLogger.new
+  logger.log
 end
