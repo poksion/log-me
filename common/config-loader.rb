b3a3101eeb7c0f6cb0a28c1f24184eabfb185c20
poksion <poksion@gmail.com>
2016-06-14 14:03:15 +0900

    Applying all cmd on dn with config loader

--- common/config-loader.rb
+++ common/config-loader.rb
@@ -1,3 +1,6 @@
+# encoding: utf-8
+# vim:tabstop=2 softtabstop=2 expandtab shiftwidth=2:
+
 require_relative 'lib/os-checker'
 require 'yaml'
 
@@ -15,6 +18,8 @@ class ConfigLoader
     @log_dir_fullpath = File.expand_path(paths['log_dir'])
     @log_file_fullpath = File.expand_path(paths['log_file'])
     @blog_dir_fullpath = File.expand_path(paths['blog_dir'])
+    @notes_dir_fullpath = File.expand_path(paths['notes_dir'])
+    @box_working_dir_fullpath = File.expand_path(paths['box_working_dir'])
   end
   
   def get_parent_dir_fullpath
@@ -36,7 +41,19 @@ class ConfigLoader
   def get_blog_dir_fullpath
     @blog_dir_fullpath
   end
-    
+  
+  def get_notes_dir_fullpath
+    @notes_dir_fullpath
+  end
+  
+  def get_box_working_dir_fullpath
+    @box_working_dir_fullpath
+  end
+  
+  def get_log_ics_fullpath
+    File.join(@log_dir_fullpath, 'backup-calendar')
+  end
+
 end
 
 if __FILE__ == $0
@@ -49,6 +66,11 @@ if __FILE__ == $0
     File.join(config_loader.get_parent_dir_fullpath, "mdblog-metadata-generator/project-metadata-generator.rb")
   when 'blog-dir'
     config_loader.get_blog_dir_fullpath
+  when 'notes-public-box-dir'
+    public_dir = File.dirname(config_loader.get_blog_dir_fullpath())
+    "#{config_loader.get_notes_dir_fullpath} #{public_dir} #{config_loader.get_box_working_dir_fullpath}"
+  when 'log-ics-dir'
+    config_loader.get_log_ics_fullpath
   else
     'check-parameter'
   end
