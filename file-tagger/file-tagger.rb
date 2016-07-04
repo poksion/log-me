4c1969cd5b82bf95862577735d681a38a5895367
poksion <poksion@gmail.com>
2016-07-05 02:42:23 +0900

    Renaming photo-manager to file-tagger

--- file-tagger/file-tagger.rb
+++ file-tagger/file-tagger.rb
@@ -3,16 +3,9 @@
 
 require 'digest'
 
-#id: hash-code
-# - full_path
-# - file_name: (진짜파일이름, 사진/그밑.. 이렇게 나타냄) (태그와 (-_-빼고))
-# - tags: (dropbox_2015_-_-file_name.jpg)
-# - date:
-# - location: 
-
 if __FILE__ == $0
   cnt = 0
-  parent_dir = '/your/photo/directory/'
+  parent_dir = '/loading/from/config/'
 #  Dir.glob(parent_dir + '**/*', File::FNM_DOTMATCH) do |f|
   Dir.glob(parent_dir + '**/*') do |f|
     next unless File.file?(f)
