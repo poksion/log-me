5f06622af0542b0e30c60ad2788e300b18588c5e
poksion <poksion@gmail.com>
2016-07-06 02:45:49 +0900

    basic implementing tagger-reporter

--- file-tagger/file-tagger.rb
+++ file-tagger/file-tagger.rb
@@ -1,19 +1,11 @@
 # encoding: utf-8
 # vim:tabstop=2 softtabstop=2 expandtab shiftwidth=2:
 
-require 'digest'
+require_relative 'lib/tagger-reporter'
 
 def report_tagger(config_filename)
-  cnt = 0
-  parent_dir = '/loading/from/config/'
-#  Dir.glob(parent_dir + '**/*', File::FNM_DOTMATCH) do |f|
-  Dir.glob(parent_dir + '**/*') do |f|
-    next unless File.file?(f)
-    puts f.gsub(parent_dir, '')
-    puts Digest::MD5.file(f).hexdigest
-    cnt += 1
-  end
-  puts "total files : #{cnt}"
+  tagger_reporter = TaggerReporter.new(config_filename)
+  tagger_reporter.report()
 end
 
 def compare_result(result_0, result_1)
@@ -28,10 +20,13 @@ if __FILE__ == $0
 
   valid_argv = true
   report_mode = true
-  config_file = "file-tagger-config.yml"
-  if ARGV.length == 1
+  config_file = ""
+
+  if ARGV.length == 0
+    config_file = ""
+  elsif ARGV.length == 1
     config_file = ARGV[0]
-  elsif RGV.length == 3
+  elsif ARGV.length == 3
     valid_argv = false unless ARGV[0] == "compare"
     result_0 = ARGV[1]
     result_1 = ARGV[2]
--- file-tagger/lib/tagger-reporter.rb
+++ file-tagger/lib/tagger-reporter.rb
@@ -7,26 +7,128 @@ require 'digest'
 class TaggerReporter
   
   def initialize(config_filename)
-    file = File.expand_path(config_filename)
+    
+    @parent_path = File.dirname(File.dirname(__FILE__))
+
+    if config_filename == nil or config_filename.empty?
+      config_filename = File.join(@parent_path, 'file-tagger-config.yml')
+    end
+
+    config_file_path = File.expand_path(config_filename)
     contents = YAML.load_file(config_file_path)
     root = contents['file_tagger_config']
-
+    src = root['src']
+      
     @tagger_id = root['tagger_id']
     
-    src = root['src']
-    @src_root_path = File.expand_path(src['root_path'])
+    @src_root_path = File.expand_path(src['root_path']) + '/'
+
+    @src_use_multiple_result = src['use_multiple_result']
+    @src_multiple_result_type = src['multiple_result_type']
+    @src_paging_item_count = src['paging_item_count']
+    
+    @src_hash_limit = src['hash_limit']
 
   end
   
-  def report()
-    cnt = 0
-    Dir.glob(@src_root_path + '**/*') do |f|
-      next unless File.file?(f)
-      puts f.gsub(@src_root_path, '')
-      puts Digest::MD5.file(f).hexdigest
-      cnt += 1
+  def get_result_map
+    result_map = Hash.new
+    unless @src_use_multiple_result
+      result_map["#{@tagger_id}-result.yml"] = @src_root_path + "**/*"
+      return result_map
     end
-  puts "total files : #{cnt}"
+    
+    # directory
+    if @src_multiple_result_type == 'directory'
+      Dir.glob(@src_root_path + '*/') do |f|
+        next unless File.directory?(f)
+        result_name = f.gsub(@src_root_path, '')[0...-1] + "-result.yml"
+        result_glob = f + "**/*"
+        result_map[result_name] = result_glob
+      end
+      return result_map
+    end
+    
+    # paging
+    if @src_multiple_result_type == 'paging'
+    end
+    
+    return result_map
+  end
+  
+  def get_id(f)
+    limit_size = @src_hash_limit[0...-2].to_i * 1000 * 1000
+    if File.stat(f).size > limit_size
+      md5 = Digest::MD5.new
+      return md5.update(f).hexdigest + "_name"
+    end
+    
+    return Digest::MD5.file(f).hexdigest
+  end
+  
+  def get_file_name(f)
+    f.gsub(@src_root_path, '')
+  end
+  
+  def get_tags(f)
+    tags = get_file_name(f).split('/')
+    return tags.first(tags.size-1).join(", ")
+  end
+  
+  def check_duplicated_and_append(item, id_group, duplicated_items)
+      item_id = item['id']
+      group_cnt = id_group[item_id].size
+      if group_cnt > 0
+        duplicated_items << item_id
+        return true
+      end
+      
+      return false
+  end
+  
+  def write_result(result_name, result)
+    org_cnt = result.size
+    puts org_cnt
+    
+    id_group = Hash.new { |hash,key| hash[key] = [] }
+    result.each { |a| id_group[ a['id'] ] << a }
+    
+    duplicated_items = Array.new
+    result.delete_if { |i| check_duplicated_and_append(i, id_group, duplicated_items) }
+      
+    puts duplicated_items.size
+    puts result.size
+      
+    result.sort! {|a, b| a['file_name'] <=> b['file_name']}
+      
+    result_file_fullpath = File.expand_path( File.join(@parent_path, 'result', result_name) )
+    File.open(result_file_fullpath, 'w+') do |f|
+      f.puts "total : #{org_cnt}"
+      result.each do | item |
+        f.puts "---"
+        f.puts "id : #{item['id']}"
+        f.puts "  - file_name : #{item['file_name']}"
+        f.puts "  - tags : #{item['tags']}"
+      end
+    end
+  end
+  
+  def report()
+    
+    result_map = get_result_map
+    result_map.each do | result_name, result_glob|
+      result = Array.new
+      Dir.glob(result_glob) do |f|
+        next unless File.file?(f)
+        result_item = Hash.new
+        result_item['id'] = get_id(f)
+        result_item['file_name'] = get_file_name(f)
+        result_item['tags'] = get_tags(f)
+        result << result_item
+      end
+      write_result(result_name, result)
+    end
+
   end
 
 end
