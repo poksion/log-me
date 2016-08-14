9b3bf7dc4f229a7e78c80aff7cb5efe1e5265e0e
poksion <poksion@gmail.com>
2016-08-15 01:49:57 +0900

    Implementing file-tagger "dup" operation

--- file-tagger/lib/tagger-operator.rb
+++ file-tagger/lib/tagger-operator.rb
@@ -8,40 +8,96 @@ require_relative 'tagger-res-result'
 # op_type
 # sub : subtraction
 # dup : duplication
-class TaggerOpSub
+class TaggerOpBase
 
   def show(lhs_result, rhs_result)
+    on_begin
     
-    result = Array.new
-
     lhs_result.each do | lhs_first_depth |
       lhs_item = lhs_first_depth['item']
       next if  lhs_item == nil
-      found_lhs = false
+      breaked_rhs_item = nil
       rhs_result.each do | rhs_first_depth |
         rhs_item = rhs_first_depth['item']
         next if rhs_item == nil
-        break if ( found_lhs = (lhs_item[0]['id'] == rhs_item[0]['id']) )
+        breaked_rhs_item = on_compare_item_and_break_if(lhs_item, rhs_item)
+        break if breaked_rhs_item != nil
       end
-      result << lhs_item[2]['file_full_path'] unless found_lhs
+
+      on_lhs_loop_end(lhs_item, breaked_rhs_item)
     end
     
-    if result.size == 0
+    on_end
+  end
+  
+  def on_begin
+  end
+  
+  def on_compare_item_and_break_if(lhs_item, rhs_item)
+    return nil
+  end
+  
+  def on_lhs_loop_end(lhs_item, breaked_rhs_item)
+  end
+  
+  def on_end
+  end
+
+end
+  
+class TaggerOpSub < TaggerOpBase
+  
+  def on_begin
+    @result = Array.new
+  end
+
+  def on_compare_item_and_break_if(lhs_item, rhs_item)
+    return lhs_item[0]['id'] == rhs_item[0]['id'] ? rhs_item : nil
+  end
+
+  def on_lhs_loop_end(lhs_item, breaked_rhs_item)
+    if breaked_rhs_item == nil
+      @result << lhs_item[2]['file_full_path']
+    end
+  end
+  
+  def on_end
+    if @result.size == 0
       puts "not found"
     else
-      result.each do |file_name|
+      @result.each do |file_name|
         puts file_name
       end
     end
-
   end
 
 end
 
-class TaggerOpDup
-  def show(report_cols, lhs_result, rhs_result)
-    puts "not implemented yet"
+class TaggerOpDup < TaggerOpBase
+  def on_begin
+    @result = Array.new(2) { Array.new }
   end
+
+  def on_compare_item_and_break_if(lhs_item, rhs_item)
+    if lhs_item[0]['id'] == rhs_item[0]['id']
+      @result[0] << lhs_item[2]['file_full_path']
+      @result[1] << rhs_item[2]['file_full_path']
+    end
+    # non-break
+    return nil
+  end
+  
+  def on_end
+    total_cnt = @result[0].size
+    if total_cnt == 0
+      puts "not found"
+    else
+      (1..total_cnt).each do |i|
+        puts "#{@result[0][i-1]}  -  #{@result[1][i-1]}"
+      end
+    end
+  end
+
 end
 
 class TaggerOperator
