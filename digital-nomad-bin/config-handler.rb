82539218e7af868a1a0e9a50138ba28d301d89a6
poksion <poksion@gmail.com>
2017-01-19 13:26:59 +0900

    ignore gpre for  non-configured path

--- digital-nomad-bin/config-handler.rb
+++ digital-nomad-bin/config-handler.rb
@@ -3,6 +3,10 @@
 
 require_relative '../common/lib/config-loader'
 
+def refine_dir(dir_name)
+  File.exists?(dir_name) ? dir_name : ""
+end
+
 if __FILE__ == $0
   config_loader = ConfigLoader.new
 
@@ -12,8 +16,10 @@ if __FILE__ == $0
   when 'blog-dir'
     config_loader.get_blog_dir_fullpath
   when 'notes-public-box-dir'
-    public_dir = File.dirname(config_loader.get_blog_dir_fullpath())
-    "#{config_loader.get_notes_dir_fullpath} #{public_dir} #{config_loader.get_box_working_dir_fullpath}"
+    notes_dir = refine_dir( config_loader.get_notes_dir_fullpath )
+    public_dir = refine_dir( File.dirname(config_loader.get_blog_dir_fullpath) )
+    box_dir = refine_dir( config_loader.get_box_working_dir_fullpath )
+    "#{notes_dir} #{public_dir} #{box_dir}"
   when 'log-ics-dir'
     config_loader.get_log_ics_fullpath
   else
