0cf21b5b967ea30d80b7eb799e6eae908a3af9f5
poksion <poksion@gmail.com>
2017-02-01 11:04:33 +0900

    fix crontab bug on daily-auto-commit

--- scheduler/crontab-adder.rb
+++ scheduler/crontab-adder.rb
@@ -5,7 +5,7 @@ if __FILE__ == $0
   crontab_list = `crontab -l`
   if crontab_list == nil or not crontab_list.include?("worklog")
     echo_worklog = 'echo "* * * * * /usr/bin/ruby ~/workspace/log-me/worklog/worklog.rb"'
-    echo_daily_auto_commit = 'echo "5 6-22 * * * /usr/bin/ruby ~/workspace/log-me/daily-auto-commit/daily-auto-committer.rb"'
+    echo_daily_auto_commit = 'echo "*/5 6-22 * * * /usr/bin/ruby ~/workspace/log-me/daily-auto-commit/daily-auto-committer.rb"'
     `(crontab -l ; #{echo_worklog} ; #{echo_daily_auto_commit} ) | crontab -`
   end
 end
