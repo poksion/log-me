89b5226da03fae5aac80cdb01af322bc51d13c75
poksion <poksion@gmail.com>
2017-10-12 12:59:20 +0900

    refactoring scheduler installer - extract helper to agent

--- scheduler/installer/mac-fyac-server-installer.rb
+++ scheduler/installer/mac-fyac-server-installer.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 # vim:tabstop=2 softtabstop=2 expandtab shiftwidth=2:
 
-require_relative '../mac-installer-lib'
+require_relative '../agent/mac-agent-adder'
 
 if __FILE__ == $0
   installer = MacInstaller.new
