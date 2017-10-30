ffaaa1695606e099ca933b501e5266a66895f41c
poksion <poksion@gmail.com>
2017-10-31 08:37:07 +0900

    add bash-profile to symbolic-path-installer

--- scheduler/installer/symbolic-path-installer.rb
+++ scheduler/installer/symbolic-path-installer.rb
@@ -2,13 +2,27 @@
 # vim:tabstop=2 softtabstop=2 expandtab shiftwidth=2:
 
 require_relative '../../common/lib/config-loader'
+require_relative '../../common/lib/os-checker'
+
+class OsSpecific
+  include OsChecker
+  
+  def run(live_config)
+    if run_on_mac?
+      `rm -rf ~/.bash_profile && ln -s #{live_config}/mac-bash-profile ~/.bash_profile`
+    end
+  end
+end
 
 if __FILE__ == $0
   config_loader = ConfigLoader.new
+  os_specific = OsSpecific.new
+
   live_config = config_loader.get_raw_paths['live_config']
   if live_config != nil
     `ln -s #{live_config}/vim ~/.vim`
     `ln -s #{live_config}/vimrc ~/.vimrc`
+    os_specific.run(live_config)
   end
 
   notes_dir = config_loader.get_raw_paths['notes_dir']
